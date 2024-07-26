c6881e838824299985718af34d9fe4fc2c37814b
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-07-25 22:58:39 -0700

    added more emits like send and leave room

--- index.js
+++ index.js
@@ -7,12 +7,30 @@ const io = require("socket.io")(3001, {
 io.on("connection", (socket) => {
   console.log(socket.id);
 
-  socket.on("createOrJoin", (roomID) => {
+  socket.on("createOrJoin", (roomID, message) => {
     const room = io.sockets.adapter.rooms.get(roomID);
 
-    if (room) console.log(`Joining existing room: ${roomID}`);
-    else console.log(`Creating new room: ${roomID}`);
+    if (room) message(`Joining existing room ${roomID}`);
+    else message(`Creating new room ${roomID}`);
 
     socket.join(roomID);
+  });
+
+  socket.on("leaveRoom", (roomID, message) => {
+    const room = io.sockets.adapter.rooms.get(roomID);
+
+    if (room) {
+      message(`Leaving room ${roomID}`);
+      socket.leave(roomID);
+    } else message("Error leaving room, currently not in a room.");
+  });
+
+  socket.on("sendMessage", (roomID, message, cbMessage) => {
+    const room = io.sockets.adapter.rooms.get(roomID);
+
+    if (room) {
+      cbMessage(`Sending message to current room ${roomID}`);
+      socket.to(roomID).emit("getMessage", message);
+    } else cbMessage("Error sending message, currently not in a room.");
   });
 });
